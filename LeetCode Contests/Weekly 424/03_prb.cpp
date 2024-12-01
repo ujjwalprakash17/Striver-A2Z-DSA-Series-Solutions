4c688d08f2a139fb09eecb015527db9ad7cc444a
Ujjwal Prakash <Ujjwalprakash144@gmail.com>
2024-12-01 23:02:26 +0530

    added few more code lines

---
+++ LeetCode Contests/Weekly 424/03_prb.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int minZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+// {
+
+// }
+
+int main()
+{
+    vector<int> v1 = {1,2,4};
+    vector<int> v2 = {1,3,2};
+    bool check = v1 > v2;
+    cout << check << endl;
+    return 0;
+}
