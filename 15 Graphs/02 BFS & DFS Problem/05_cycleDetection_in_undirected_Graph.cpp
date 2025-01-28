64ea96a79b07254ee1e64c4a07cd11b4c07720a4
Ujjwal P <Ujjwalprakash144@gmail.com>
2025-01-28 07:28:53 +0530

    revising my previous problems

--- 15 Graphs/02 BFS & DFS Problem/05_cycleDetection_in_undirected_Graph.cpp
+++ 15 Graphs/02 BFS & DFS Problem/05_cycleDetection_in_undirected_Graph.cpp
@@ -1,38 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-// Approach :
-//  1. take each of the string into set (so that it makes searching in wordlist easier)
-//  2. try solving using bfs
-//  3. insert the beginword into queue
-//  4. start the loop till the queue is not empty
-//  5. take each front word out and try to find out which word exists in set
-//  6. if found the word take the word out from the queue and also from set data structure
-//  7. that is all
-
-int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+// cycle detection in an undirected graph using bfs
+bool bfsTraversal(int node, vector<vector<int>> &adj, vector<int> &visited)
 {
-    set<string> st;
-    for (string i : wordList)
-    {
-        st.insert(i);
-    }
-    queue<pair<string, int>> q;
-    q.push({beginWord, 1});
+    queue<pair<int, int>> q;
+    //{currNode, prevNode}
+    q.push({node, -1});
+    visited[node] = 1;
     while (!q.empty())
     {
-        string currWord = q.front().first;
-        int currLvl = q.front().second;
-        // try finding for each word in set
-        for(int i = 0; i < st.size() ; i++){
-            for(int j = 0 ; j < currWord.size();  j++){
-                for(int k = 1; k < 26; k++){
-                    currWord[i] = 
-                }
+        int currNode = q.front().first;
+        int prevNode = q.front().second;
+        q.pop();
+        for (int neighbour : adj[currNode])
+        {
+            if (!visited[neighbour])
+            {
+                q.push({neighbour, currNode});
+                visited[neighbour] = 1;
             }
+            else if (visited[neighbour] && neighbour != prevNode)
+                return true;
         }
     }
+    return false;
 }
+bool isCycle(vector<vector<int>> &adj)
+{
+    // Code here
+    int n = adj.size();
+    vector<int> visited(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            if (bfsTraversal(i, adj, visited) == true)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // Approach :
+// //  1. take each of the string into set (so that it makes searching in wordlist easier)
+// //  2. try solving using bfs
+// //  3. insert the beginword into queue
+// //  4. start the loop till the queue is not empty
+// //  5. take each front word out and try to find out which word exists in set
+// //  6. if found the word take the word out from the queue and also from set data structure
+// //  7. that is all
+
+// int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+// {
+//     set<string> st;
+//     for (string i : wordList)
+//     {
+//         st.insert(i);
+//     }
+//     queue<pair<string, int>> q;
+//     q.push({beginWord, 1});
+//     while (!q.empty())
+//     {
+//         string currWord = q.front().first;
+//         int currLvl = q.front().second;
+//         // try finding for each word in set
+//         for(int i = 0; i < st.size() ; i++){
+//             for(int j = 0 ; j < currWord.size();  j++){
+//                 for(int k = 1; k < 26; k++){
+//                     currWord[i] =
+//                 }
+//             }
+//         }
+//     }
+// }
 
 int main()
 {
--- Revision.cpp
+++ Revision.cpp
@@ -1,94 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string shiftingLetters(string s, vector<vector<int>> &shifts)
+int solution(int n, vector<int> &height, vector<int> &dp)
 {
-    int n = s.size();
-    vector<int> freq(n + 2, 0);
-    for (auto it : shifts)
-    {
-        int start = it[0];
-        int end = it[1];
-        int direc = it[2];
-        if (direc == 1)
-        {
-            freq[start]++;
-            freq[end + 1]--;
-        }
-        else
-        {
-            freq[start]--;
-            freq[end + 1]++;
-        }
-    }
-    int shift = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        shift += freq[i];
-        shift %= 26;                                 // Because there are 26 letters in the alphabet
-        s[i] = (s[i] - 'a' + shift + 26) % 26 + 'a'; // Ensuring the result is within 'a' to 'z' }
-    }
-    return s;
+    if (n == 0)
+        return 0;
+    int oneStep = INT_MAX;
+    int twoStep = INT_MAX;
+    if(dp[n] != -1) return dp[n];
+    if (n > 0)
+        oneStep = solution(n - 1, height, dp) + abs(height[n] - height[n - 1]);
+    if (n > 1)
+        twoStep = solution(n - 2, height, dp) + abs(height[n] - height[n - 2]);
+    return dp[n] = min(oneStep, twoStep);
+}
+int minCost(vector<int> &height)
+{
+    // Code here
+    int n = height.size();
+    vector<int> dp(n, -1);
+    return solution(n - 1, height, dp);
 }
 
 int main()
 {
-    string s = "dztz";
-    vector<vector<int>> shifts = {{0, 0, 0}, {1, 1, 1}};
-    s = shiftingLetters(s, shifts);
-    cout << s << "\n";
-    // int val = (124 % 97) % 26;
-    // cout << val << "\n";
-    return 0;
-}#include <bits/stdc++.h>
-using namespace std;
-
-vector<vector<int>> threeSum(vector<int> &nums)
-{
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-    int i = 0, j = i + 1, k = n -1;
-    set<vector<int>> ans;
-    while((k-i) > 1)
-    {
-        while(j < k)
-        {
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum < 0)
-            {
-                j++;
-            }
-            else if(sum > 0)
-            {
-                k--;
-            }
-            else{
-                vector<int> temp = {nums[i], nums[j], nums[k]};
-                ans.insert(temp);
-            }
-        }
-        i++, j = i + 1, k = n -1;
-    }
-    vector<vector<int>> result(ans.size());
-    int p = 0;
-    for(const auto &vec : ans)
-    {
-        result[p] = vec;
-    }
-    return result;
-}
-int main()
-{
-    vector<int> ans= {-1,0,1,2,-1,-4};
-    vector<vector<int>> result = threeSum(ans);
-    for(int i = 0; i < result.size(); i++)
-    {
-        for(int j = 0; j < 3 ; j++)
-        {
-            cout << result[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    vector<int> height = {10, 30, 40, 20, 50};
+    cout << minCost(height);
 
     return 0;
 }
