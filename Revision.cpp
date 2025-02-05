3c78b886d05cdecf6f225ecf1956f8ae2f7e336e
Ujjwal P <Ujjwalprakash144@gmail.com>
2025-02-05 09:31:26 +0530

    daily problems

--- Revision.cpp
+++ Revision.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isCycle(vector<vector<int>> &adj)
+int maxAscendingSum(vector<int> &nums)
 {
-    // Code here
+    int n = nums.size();
+    int ans = nums[0];
+    int sum = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] == nums[i - 1])
+            continue;
+        if (nums[i] > nums[i - 1])
+            sum += nums[i];
+        else if (nums[i] < nums[i - 1])
+            sum = nums[i];
+        ans = max(ans, sum);
+    }
+    return ans;
 }
 
 int main()
 {
+    vector<int> sample = {5,5,6,6,6,9,1,2};
+    cout << maxAscendingSum(sample);
 
     return 0;
 }
