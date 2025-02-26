70df648d6007abf4e7757641c1e22f253b2e3ef9
Ujjwal P <Ujjwalprakash144@gmail.com>
2025-02-27 00:25:04 +0530

    added a few more problems to that

---
+++ 15 Graphs/03 TopoSort and Problems/04_DetectCycleInGraph.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Detect Cycle in Directed Graph using BFS
+
+bool isCyclic(vector<vector<int>> &adj)
+{
+    // code here
+}
+
+int main()
+{
+
+    return 0;
+}
