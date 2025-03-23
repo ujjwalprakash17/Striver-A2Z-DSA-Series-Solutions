75d5e0748e9755650d0c11cc9c036816eefe847f
Ujjwal P <Ujjwalprakash144@gmail.com>
2025-03-24 01:42:35 +0530

    fixed the new code method in few of the quesions in dp on subsets

--- 16 Dynamic Programming/03 DP on Subsets/05_PartitionWithDiff.cpp
+++ 16 Dynamic Programming/03 DP on Subsets/05_PartitionWithDiff.cpp
@@ -1,68 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int solution(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
 {
-    if (ind == 0)
-    {
-        if (target == 0 && arr[0] == 0)
-            return 2;
-        if (target == 0 || arr[0] == target)
-            return 1;
-        return 0;
-    }
-    if (dp[ind][target] != -1)
-        return dp[ind][target];
+    if (ind < 0)
+        return target == 0;
+    if(dp[ind][target] != -1) return dp[ind][target];
     int notPick = solution(ind - 1, target, arr, dp);
     int pick = 0;
     if (arr[ind] <= target)
-    {
         pick = solution(ind - 1, target - arr[ind], arr, dp);
-    }
-    return dp[ind][target] = pick + notPick;
-}
-// tabulation version
-int countPartitions(vector<int> &arr, int d)
-{
-    int n = arr.size();
-    int sum = accumulate(arr.begin(), arr.end(), 0);
-    if ((d + sum) % 2 != 0)
-        return 0;
-    int target = (d + sum) / 2;
-    vector<vector<int>> dp(n, vector<int>(target + 1, 0));
-
-    // transforming base cases in tabulation
-    dp[0][0] = 1;
-    if (arr[0] <= target)
-        dp[0][arr[0]] = 1;
-    if (arr[0] == 0)
-        dp[0][0] = 2;
-    for(int ind = 1 ; ind < n ; ind++)
-    {
-        for(int newTarget = 0; newTarget <= target ; newTarget++)
-        {
-            int notPick = dp[ind -1][newTarget];
-            int pick = 0;
-            if(arr[ind] <= newTarget)
-            {
-                pick = dp[ind-1][newTarget - arr[ind]];
-            }
-            dp[ind][newTarget] = pick + notPick;
-        }
-    }
-    return dp[n-1][target];
+    return dp[ind][target]= pick + notPick;
 }
 
 int countPartitions(vector<int> &arr, int d)
 {
     // Code here
-    int sum = accumulate(arr.begin(), arr.end(), 0);
-    if ((d + sum) % 2 != 0)
-        return 0;
-    int target = (d + sum) / 2;
+    int totalSum = 0;
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-    return solution(n - 1, target, arr, dp);
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += arr[i];
+    }
+    if ((totalSum + d) % 2 != 0 || totalSum < d) 
+    return 0;
+    int target = (totalSum + d) / 2;
+    vector<vector<int>> dp(n, vector<int>(target+1, -1));
+    return solution(n- 1, target, arr, dp);
+}
+
+
+int countPartitions(vector<int> &arr, int d)
+{
+    // Code here
+    int totalSum = 0;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        totalSum += arr[i];
+    }
+    if ((totalSum + d) % 2 != 0 || totalSum < d) 
+    return 0;
+    int target = (totalSum + d) / 2;
+    vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+    dp[0][0] = 1;
+    for(int ind = 1 ; ind <= n  ; ind++)
+    {
+        for(int t = 0; t <= target ; t++)
+        {
+            int notPick = dp[ind-1][t];
+            int pick = 0;
+            if(arr[ind-1] <= t)
+            pick = dp[ind-1][t-arr[ind-1]];
+            dp[ind][t] = pick + notPick;
+        }
+    }
+    return dp[n][target];
 }
 
 int main()
--- 16 Dynamic Programming/03 DP on Subsets/07_coinchange.cpp
+++ 16 Dynamic Programming/03 DP on Subsets/07_coinchange.cpp
@@ -1,72 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(int ind, int amt, vector<int> &coins, vector<vector<int>> &dp)
-{
-    // base case
-    if (amt == 0)
-        return 0;
-    if (ind == 0)
-    {
-        if (amt % coins[ind] == 0)
-            return (amt / coins[ind]);
-        return 1e9;
-    }
-    if (dp[ind][amt] != -1)
-        return dp[ind][amt];
-    int pick = 1e9;
-    if (coins[ind] <= amt)
-        pick = 1 + solution(ind, amt - coins[ind], coins, dp);
-    int notPick = solution(ind - 1, amt, coins, dp);
 
-    return dp[ind][amt] = min(pick, notPick);
+int solution(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp)
+{
+    if (ind < 0)
+        return amount == 0 ? 0 : 1e9;
+    if (dp[ind][amount] != -1)
+        return dp[ind][amount];
+    int notPick = solution(ind - 1, amount, coins, dp);
+    int pick = 1e9;
+    if (coins[ind] <= amount)
+        pick = 1 + solution(ind, amount - coins[ind], coins, dp);
+    return dp[ind][amount] = min(pick, notPick);
 }
 
 int coinChange(vector<int> &coins, int amount)
 {
     int n = coins.size();
-    vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-    int val = solution(n - 1, amount, coins, dp);
+    vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+    int val = solution(n-1, amount, coins, dp);
+    if(val == 1e9) return -1;
+    return val;
+}
+
+int coinChange(vector<int>& coins, int amount) {
+    int n = coins.size();
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
+    // base case transformation
+    dp[0][0] = 0;
+    for (int ind = 1; ind <= n; ind++) {
+        for (int amt = 0; amt <= amount; amt++) {
+            int notPick = dp[ind - 1][amt];
+            int pick = 1e9;
+            if (coins[ind - 1] <= amt)
+                pick = 1 + dp[ind][amt - coins[ind - 1]];
+            dp[ind][amt] = min(pick, notPick);
+        }
+    }
+    int val = dp[n][amount];
     if (val == 1e9)
         return -1;
     return val;
 }
-
-
-int coinChange(vector<int> &coins, int amount)
-{
-    int n = coins.size();
-    vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
-    //handle base cases correctly first
-    for(int i = 0; i < n;  i++)
-    {
-        dp[i][0] = 0;
-    }
-    for(int amt = 0; amt <= amount ; amt++)
-    {
-        if(amt % coins[0] == 0)
-        dp[0][amt] = amt / coins[0];
-        else{
-            dp[0][amt] = 1e9;
-        }
-    }
-
-    for(int ind = 1 ; ind < n ; ind++)
-    {
-        for(int amt = 1 ; amt <= amount ; amt++)
-        {
-            int pick = 1e9;
-            if(coins[ind] <= amt)
-            pick = 1 + dp[ind][amt-coins[ind]];
-            int notPick = dp[ind-1][amt];
-            dp[ind][amt] = min(pick, notPick);
-        }
-    }
-    return dp[n-1][amount] == 1e9 ? -1 : dp[n-1][amount];
-    //return -1 if ans is 1e9
-}
-
-
 int main()
 {
 
