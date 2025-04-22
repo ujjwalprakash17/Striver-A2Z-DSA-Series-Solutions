86936ec9da7b65780c0ac2f89e4ac9e811fdaec1
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-04-22 12:44:57 +0530

    Longest increasing subsequence memoization

--- 15 Graphs/GraphRevision.cpp
+++ 15 Graphs/GraphRevision.cpp
@@ -1,9 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
-{
-    
+vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int m = mat[0].size();
+
+    // we know the distance between the cells which are 0 from itself is 0
+    //so push the blocks which are initially 0
+    queue<pair<pair<int, int>, int>> q;
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = 0 ; j < m ; j++)
+        {
+            if(mat[i][j] == 0){
+                q.push({{i, j}, 0});
+            }
+        }
+    }
+
+    while(!q.empty())
+    {
+        int row = q.front().first.first;
+        int col = q.front().first.second;
+        int dist = q.front().second;
+        q.pop();
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+        for(int i = 0; i < 4; i++)
+        {
+            int nRow = delRow[i] + row;
+            int nCol = delCol[i] + col;
+            if(nRow >=0 && nRow < n && nCol >= 0 && nCol < m && )
+        }
+    }
+
 }
 
 int main()
---
+++ 16 Dynamic Programming/07_DP on LIS/01_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int ind, int prev, vector<int> &nums, vector<vector<int>>&dp)
+{
+    if(ind >= nums.size() ) return 0;
+    if(dp[ind][prev+1] != -1) return dp[ind][prev+1]; 
+    int notPick = solution(ind + 1, prev, nums, dp);
+    int pick = 0;
+    if(prev == -1 || nums[prev] < nums[ind]){
+        pick = 1 + solution(ind + 1, ind, nums, dp);
+    }
+    return dp[ind][prev+1] = max(pick, notPick);
+}
+
+int lengthOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>>dp(n, vector<int>(n+1, -1));
+    return solution(0, -1, nums, dp);
+}
+
+int main()
+{
+
+    return 0;
+}
