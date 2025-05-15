5135200f138334fc089af5d9dd1393ee271c678f
Ujjwal P <Ujjwalprakash144@gmail.com>
2025-05-15 23:27:28 +0530

    fixed the longest increasing subsequence problem

--- 16 Dynamic Programming/DP_Revision.cpp
+++ 16 Dynamic Programming/DP_Revision.cpp
@@ -1,27 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(int ind, int prev, vector<int> &nums, vector<vector<int>> &dp)
+bool solution(int ind, int target, vector<int> &arr, vector<vector<int>> &dp)
 {
-    int n = nums.size();
-    if(ind >= n){
-        return 0;
-    }
-    if(dp[ind][prev+1] != -1) return dp[ind]
-    int notPick = solution(ind + 1, prev, nums, dp);
-    int pick = 0;
-    if(prev == -1 || nums[prev] < nums[ind])
+    if (ind == -1 )
     {
-        pick = 1 + solution(ind + 1, ind, nums, dp);
+        return target == 0;
     }
-    return dp[ind][prev+1] = max(pick, notPick);
+    if(dp[ind][target] != -1) return dp[ind][target];
+    bool notPick = solution(ind - 1, target, arr, dp);
+    bool pick = false;
+    if (target >= arr[ind])
+        pick = solution(ind - 1, target - arr[ind], arr, dp);
+    return dp[ind][target] = pick || notPick;
 }
 
-int lengthOfLIS(vector<int> &nums)
+bool isSubsetSum(vector<int> &arr, int sum)
+{
+    // code here
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int> (sum + 1, -1));
+    return solution(n-1, sum, arr, dp);
+}
+
+bool isSubsetSum(vector<int> &arr, int sum)
+{
+    // code here
+    int n = arr.size();
+    vector<vector<int>> dp(n+1, vector<int> (sum + 1, 0));
+
+    for(int i = 0; i <= n ; i++){
+        dp[i][0] = 1;
+    }
+
+    for(int ind = 1; ind <= n ; ind++){
+        for(int target = 0; target <= sum ; target++){
+            bool notPick = dp[ind-1][target];
+            bool pick = false;
+            if(target >= arr[ind]){
+                pick = dp[ind-1][target-arr[ind]]
+            }
+            dp[ind][target] = pick || notPick;
+        }
+    }
+    return dp[n][sum];
+}
+
+bool solution(int target, vector<int> &nums){
+
+    int n = nums.size();
+    vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+    for(int i = 0; i <= n ; i++)
+    {
+        dp[i][0] = 1;
+    }
+
+    for(int ind = 1; ind <= n ; ind++)
+    {
+        for(int sum = 1 ; sum <= target ; sum++)
+        {
+            int notPick = dp[ind-1][sum];
+            int pick = 0;
+            if(nums[ind] <= sum)
+            pick = dp[ind-1][sum-nums[ind]];
+            dp[ind][sum] = pick | notPick;
+        }
+    }
+    return dp[n][target];
+
+}
+
+bool canPartition(vector<int> &nums)
 {
     int n = nums.size();
-    vector<vector<int>> dp(n, vector<int> (n+1, -1));
-    return solution(0, -1, nums, dp);
+    int totalSum = 0;
+    for(int val : nums){
+        totalSum += val;
+    }
+    if(totalSum % 2 != 0 )return false;
+    int newTarget = totalSum / 2;
+    return solution (newTarget, nums);
 }
 
 int main()
@@ -29,4 +88,3 @@ int main()
 
     return 0;
 }
-
