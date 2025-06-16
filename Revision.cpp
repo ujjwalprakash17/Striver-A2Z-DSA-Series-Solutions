76b87ee80c97f19496af5749f328615c05e66731
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-06-16 18:07:33 +0530

    revised a few problems

--- Revision.cpp
+++ Revision.cpp
@@ -2,26 +2,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solution(int ind, int target, vector<int> &nums)
-{
-   
-    if(ind < 0)
-        return target == 0;
-    
-    int takePos = solution(ind-1, target + nums[ind], nums);
-    int takeNeg = solution(ind - 1, target - nums[ind], nums);
-    return takePos + takeNeg;
-}
-
-int findTargetSumWays(vector<int> &nums, int target)
-{   
-    int n = nums.size();
-    
-    return solution(n-1, target, nums);
-}
-
 int main()
 {
+    int val = 12345;
+    int ans = log10(val) + 1;
+    cout << ans;
 
     return 0;
 }
