0bfe5ecd0cf3fafac4a98157b4e39c5036f7820c
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-07-29 12:25:24 +0530

    Added code for cycle detection in graph using DFS

--- 15 Graphs/03 TopoSort and Problems/04_DetectCycleInGraph.cpp
+++ 15 Graphs/03 TopoSort and Problems/04_DetectCycleInGraph.cpp
@@ -1,11 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Detect Cycle in Directed Graph using BFS
-
-bool isCyclic(vector<vector<int>> &adj)
+//Cycle Detection in graph using dfs
+bool dfsTraversal(int start, vector<int> &visited, vector<int> &currPath, vector<vector<int>> &adjList)
 {
-    // code here
+    visited[start] = 1;
+    currPath[start] = 1;
+    for(int neighbour : adjList[start])
+    {
+        if(!visited[neighbour])
+        {
+            if(dfsTraversal(neighbour, visited, currPath, adjList)) return true;
+        }else if(currPath[neighbour]){
+            return true;
+        }
+    }
+    currPath[start] = 0;
+    return false;
+}
+
+bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+{
+    // convert this into adjList
+    vector<vector<int>> adjList(numCourses);
+    for(int i = 0; i < prerequisites.size(); i++)
+    {
+        int u = prerequisites[i][0];
+        int v = prerequisites[i][1];
+        adjList[v].push_back(u);
+    }
+    vector<int> visited(numCourses, 0);
+    vector<int> currPath(numCourses, 0);
+    //perform the cycle detection in a directed graph
+    for(int i = 0; i < numCourses; i++)
+    {
+        if(!visited[i]){
+            if(dfsTraversal(i, visited, currPath, adjList)) return false;
+        }
+    }
+    return true;
 }
 
 int main()
