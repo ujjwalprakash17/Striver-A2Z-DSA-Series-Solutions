56270070431616506885c592af8ad05ef8708759
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-08-28 13:12:23 +0530

    Revision Sum Of Min subarray

--- 09 Stacks & Queue/03 Monotonic Problems/06_SumOfMinSubarray.cpp
+++ 09 Stacks & Queue/03 Monotonic Problems/06_SumOfMinSubarray.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Thought process :
-// Bruteforce : we can find using two loops T.C - O(n * n)
 
-// <-----------------OPTIMISED APPROACH--------------->
+// Sum of Subarray minimums
+// Will discuss the thought process thoroughly
 
-// Thought process : shift the way of thinking as ki for a particular index, in how many subarrays this is minimum
-//  toh hmne ye pta kr liya ki kitne subarrays me yeh minimum hai toh hm formulae aise likhenge ki
-//  no of subarrays (in which arr[i] is minimum) * arr[i] for all i from 1 to n
+// 1. Bruteforce thinking :
+// we can find out all the subarrays and then we can find out their minimum and keep summing it
 
-// now our problem reduced to how do we find the number of subarrays in which this is minimum?
+// T.C- O(n^2) S.C - O(1)
 
-// hm left me dekhte jayenge ki jb tak current value se koi chhota na mil jaye, same hm right me bhi dekhte jayenge jb tak isse koi chhota na mil jaye
-// (this means we need to find next immediate smaller and prev immediate smaller) for each index
-// aur agar ye nikal liya toh kaam ban jayega kaise ?
-// aise :- ki individual contribution will be arr[i] * (L + 1) * (R + 1)
+// Optimisation thinking:
+// since we have to make it less than n ^ 2 so we can think of optimizing it by T.C
+
+// but how can we optimize this?
+// We need to think in the direction where we only need to traverse O(n) times
+
+// ans : if we somehow cracked this that in how many subarrays this particular element is acting as minimum then we can solve this
+
+// Now problem reduced to :
+//  find an index is acting as minimum in how many subarrays
+//  how we can do this?
+// we can do this by finding the next and previous element which smaller than the current index element
+// we need to find next/previous smaller element in an array
+// T.C - O(n) and space complexity - O(n)
 
 vector<int> prevSmaller(vector<int> &arr)
 {
     int n = arr.size();
-    vector<int> prevSmallerArr(n, -1);
+    vector<int> prevArr(n, -1);
     stack<int> stk;
     for (int i = 0; i < n; i++)
     {
@@ -29,52 +37,51 @@ vector<int> prevSmaller(vector<int> &arr)
         }
         if (!stk.empty())
         {
-            prevSmallerArr[i] = stk.top();
+            prevArr[i] = stk.top();
         }
         stk.push(i);
     }
-    return prevSmallerArr;
+    return prevArr;
 }
+
 vector<int> nextSmaller(vector<int> &arr)
 {
     int n = arr.size();
-    vector<int> nextSmallerArr(n, n);
+    vector<int> nextArr(n, n);
     stack<int> stk;
     for (int i = n - 1; i >= 0; i--)
     {
-        while (!stk.empty() && arr[stk.top()] > arr[i])
+        while (!stk.empty() && arr[stk.top()] > arr[i]) // To handle duplicate we only have included > not >=
+        // When you have duplicates, you need to decide which occurrence gets to "claim" the subarrays containing those duplicates.
+        // consider this example [3,1,2,1] and perform dry run, a single sub array we be getting counted twice if we used >=
         {
             stk.pop();
         }
         if (!stk.empty())
-        {
-            nextSmallerArr[i] = stk.top();
-        }
+            nextArr[i] = stk.top();
         stk.push(i);
     }
-    return nextSmallerArr;
-}
-int sumSubarrayMins(vector<int> &arr)
-{
-    int MOD = 1e9 + 7;
-    int result = 0;
-    int n = arr.size();
-    vector<int> prevArray = prevSmaller(arr);
-    vector<int> nextArray = nextSmaller(arr);
-    for (int i = 0; i < n; i++)
-    {
-        int L = i - prevArray[i] - 1;
-        int R = nextArray[i] - i - 1;
-        long long count = (long long)(L + 1) * (R + 1);
-        result = (result + (count * arr[i]) % MOD) % MOD;
-    }
-    return result;
+    return nextArr;
 }
 
+int sumSubarrayMins(vector<int> &arr)
+{
+    int n = arr.size();
+    int MOD = 1e9 + 7;
+    vector<int> prevArr = prevSmaller(arr);
+    vector<int> nextArr = nextSmaller(arr);
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int L = i - prevArr[i] - 1;
+        int R = nextArr[i] - i - 1;
+        long long count = (long long)(L + 1) * (R + 1);
+        total = (total + (arr[i] * count) % MOD) % MOD;
+    }
+    return total;
+}
 int main()
 {
-    vector<int> sample = {9, 12, 10, 15, 17, 7};
-    cout << sumSubarrayMins(sample) << "\n";
 
     return 0;
 }
