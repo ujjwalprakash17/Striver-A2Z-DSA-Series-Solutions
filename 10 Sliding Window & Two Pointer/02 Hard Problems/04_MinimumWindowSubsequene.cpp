16d32e7532a3f0fdadaba8bd5a8964ef32b40bd7
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-09-05 15:55:29 +0530

    Minimum window subsequence

--- 10 Sliding Window & Two Pointer/02 Hard Problems/04_MinimumWindowSubsequene.cpp
+++ 10 Sliding Window & Two Pointer/02 Hard Problems/04_MinimumWindowSubsequene.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach for this problem is :
+//  1. increase the window length till the subsequence is found, direction : left to right
+//  2. as we found our correct window length then we will start shrinking from right to left
+//  3. And then we will update our min Length value
+// 4. as soon as we found a valid substring with minimum length then we will update our min length and ans array
+// 5. after the updatation process is complete then we will move the right again starting from left + 1
+
 string minWindow(string s, string t)
 {
     int n = s.size(), m = t.size();
@@ -30,14 +37,14 @@ string minWindow(string s, string t)
                         break;
                     start--;
                 }
-                //now our end is at end of substring and start is at start of substring then
-                if(end - start + 1 < minLength)
+                // now our end is at end of substring and start is at start of substring then
+                if (end - start + 1 < minLength)
                 {
                     minLength = end - start + 1;
                     ans = s.substr(start, minLength);
                 }
-                // we need to reset our right and t pointer 
-                right = start;//now we will check for the elements next to start
+                // we need to reset our right and t pointer
+                right = start; // now we will check for the elements next to start
                 tPointer = 0;
             }
         }
