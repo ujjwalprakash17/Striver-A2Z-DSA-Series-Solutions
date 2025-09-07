efdbcedc0242921fcf1619dacaea45eda2a3acc2
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-09-08 00:25:56 +0530

    Lemonade Change

--- 12 Greedy Algorithms/01 Easy/04_LemonadeChange.cpp
+++ 12 Greedy Algorithms/01 Easy/04_LemonadeChange.cpp
@@ -1,43 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool lemonadeChange(vector<int>& bills) 
+bool lemonadeChange(vector<int> &bills)
 {
-    int fiveCnt = 0, tenCnt = 0, twentyCnt = 0;
-    int i = 0;
     int n = bills.size();
-    while(i < n){
-        if(bills[i] == 5) fiveCnt++;
-        else if(bills[i] == 10){
-            if(fiveCnt < 1) return false;
-            fiveCnt--;
-            tenCnt++;
-        }
-        else if(bills[i] == 20)
+    int coinsCount[] = {0, 0, 0};
+    for (int bill : bills)
+    {
+        if (bill == 5)
         {
-            if(fiveCnt < 3 && tenCnt < 1) return false;
-            else if(tenCnt >= 1){
-                tenCnt--;
-                if(fiveCnt < 1) return false;
-                fiveCnt--;
-                twentyCnt++;
-            }
-            else if(fiveCnt >= 3)
-            {
-                fiveCnt -= 3;
-                twentyCnt++;
-            }
+            coinsCount[0]++;
         }
-        i++;
+        else if (bill == 10)
+        {
+            coinsCount[0]--;
+            coinsCount[1]++;
+        }
+        else if (bill == 20)
+        {
+            // case 1 : if 10 is present
+            if (coinsCount[1] > 0)
+            {
+                coinsCount[1]--;
+                coinsCount[0]--;
+            }
+            else
+            {
+                coinsCount[0] -= 3;
+            }
+            coinsCount[2]++;
+        }
+
+        if (coinsCount[0] < 0 || coinsCount[1] < 0 || coinsCount[2] < 0)
+            return false;
     }
     return true;
 }
 
 int main()
 {
-    vector<int> sample = {5,5,5,10,5,5,10,20,20,20};
-     lemonadeChange(sample) ? cout << "true" << endl : cout << "false" << endl;
+    vector<int> sample = {5, 5, 5, 10, 5, 5, 10, 20, 20, 20};
+    lemonadeChange(sample) ? cout << "true" << endl : cout << "false" << endl;
 
-    
     return 0;
 }
