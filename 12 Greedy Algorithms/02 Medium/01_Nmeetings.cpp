abcd0efaf88c6ba814a1564cd0da55631178251a
Ujjwal P <ujjwal.prakash@thinktaurus.com>
2025-09-08 16:53:41 +0530

    N meetings in a room

--- 12 Greedy Algorithms/02 Medium/01_Nmeetings.cpp
+++ 12 Greedy Algorithms/02 Medium/01_Nmeetings.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxMeetings(int n, int start[], int end[])
+int maxMeetings(vector<int> &start, vector<int> &end)
 {
-    // Your code here
-    vector<pair<int, int>> vec(n);
+    int n = start.size();
+    vector<pair<int, int>> meetings;
     for (int i = 0; i < n; i++)
     {
-        vec[i] = make_pair(start[i], end[i]);
+        meetings.push_back({start[i], end[i]});
     }
-    sort(vec.begin(), vec.end(), [](const pair<int, int> &a, const pair<int, int> &b){
-        return a.second < b.second;
-    });
+    sort(meetings.begin(), meetings.end(), [](pair<int, int> &a, pair<int, int> &b)
+         { return a.second < b.second; });
     int cnt = 1;
-    int prev = vec[0].second;
-    for(int i = 1; i < n; i++)
+    int prevEndTime = meetings[0].second;
+    for (int i = 1; i < n; i++)
     {
-        if(vec[i].first > prev)
+        if (meetings[i].first > prevEndTime)
         {
             cnt++;
-            prev = vec[i].second;
+            prevEndTime = meetings[i].second;
         }
     }
     return cnt;
-
 }
 
 int main()
 {
-    int n = 6;;
-    int arr1[] = {1, 3, 0, 5, 8, 5};
-    int arr2[] = {2, 4, 6, 7, 9, 9};
-    cout << maxMeetings(n, arr1, arr2) << endl;
+
     return 0;
 }
